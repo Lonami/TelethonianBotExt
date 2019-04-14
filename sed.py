1c43543893ab6b7dc93b31a805ad8e551f2d732e
Lonami <totufals@hotmail.com>
2019-04-14 15:51:46 +0200

    Clean up sed plugin, fix not checking old messages

--- sed.py
+++ sed.py
@@ -1,4 +1,3 @@
-# Adapted from https://github.com/SijmenSchoon/regexbot
 import re
 from collections import defaultdict, deque
 
@@ -8,84 +7,75 @@ from telethon import events
 last_msgs = defaultdict(lambda: deque(maxlen=10))
 last_replies = defaultdict(lambda: deque(maxlen=10))
 
-SED_PATTERN = r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)(/.*)?'
-PREFIX = '?sed?\n'
+SED_PATTERN = re.compile(r'^s/((?:\\/|[^/])+)/((?:\\/|[^/])*)/?(.*)')
+PREFIX = '「sed」\n'
 
 
-async def doit(message, match):
-    # Don't ignore old messages, we don't fetch them anyway
-    fr = match.group(1)
-    to = match.group(2)
-    to = (to
-          .replace('\\/', '/')
-          .replace('\\0', '\\g<0>'))
-    try:
-        fl = match.group(3)
-        if fl is None:
-            fl = ''
-        fl = fl[1:]
-    except IndexError:
-        fl = ''
+class UnknownFlag(ValueError):
+    def __init__(self, flag):
+        super().__init__(f'unknown flag: {flag}')
+        self.flag = flag
 
-    # Build Python regex flags
+
+def build_substitute(pattern, repl, flags):
+    repl = repl.replace('\\/', '/').replace('\\0', '\\g<0>')
+
     count = 1
     flags = 0
-    for f in fl:
-        if f == 'i':
-            flags |= re.IGNORECASE
-        elif f == 'm':
-            flags |= re.MULTILINE
-        elif f == 's':
-            flags |= re.DOTALL
-        elif f == 'g':
+    for f in (flags or ''):
+        if f in 'Gg':
             count = 0
-        else:
-            await message.reply('unknown flag: {}'.format(f))
-            return
+            continue
 
-    async def substitute(original, msg):
-        if original.startswith(PREFIX):
-            original = original[len(PREFIX):]
+        try:
+            flags |= getattr(re.RegexFlag, f.upper())
+        except AttributeError:
+            raise UnknownFlag(f) from None
 
-        s, i = re.subn(fr, to, original, count=count, flags=flags)
+    def substitute(string):
+        if string.startswith(PREFIX):
+            string = string[len(PREFIX):]
+
+        s, i = re.subn(pattern, repl, string, count=count, flags=flags)
         if i > 0:
-            return await msg.reply(PREFIX + s, parse_mode=None)
+            return PREFIX + s
 
-    try:
-        if message.is_reply:
-            msg = await message.get_reply_message()
-            original = msg.raw_text
-            if original:
-                return await substitute(original, msg)
-
-        else:
-            for msg in reversed(last_msgs[message.chat_id]):
-                original = msg.raw_text
-                if original:
-                    return await substitute(original, msg)
-
-    except Exception as e:
-        await message.reply('owh :(\n' + str(e))
+    return substitute
 
 
 async def init(bot):
     @bot.on(events.NewMessage(pattern=SED_PATTERN))
-    async def test(event):
-        msg = await doit(event.message, event.pattern_match)
-        if msg:
-            last_msgs[msg.chat_id].append(msg)
+    async def handler(event):
+        if event.is_reply:
+            messages = [await event.get_reply_message()]
+        else:
+            messages = reversed(last_msgs[event.chat_id])
 
-            where = event.chat_id if event.is_channel else None
-            last_replies[where].append((event.id, msg.id))
-            raise events.StopPropagation
+        substitute = build_substitute(*event.pattern_match.groups())
+        for message in messages:
+            new = substitute(message.raw_text)
+            if new is None:
+                continue
 
-    @bot.on(events.NewMessage(pattern=r'(.*)'))
-    async def msg(event):
+            try:
+                sent = await message.reply(new, parse_mode=None)
+            except Exception as e:
+                await message.reply('owh :(\n' + str(e))
+            else:
+                last_msgs[event.chat_id].append(sent)
+                where = event.chat_id if event.is_channel else None
+                last_replies[where].append((event.id, sent.id))
+
+            break
+
+        raise events.StopPropagation
+
+    @bot.on(events.NewMessage)
+    async def handler(event):
         last_msgs[event.chat_id].append(event.message)
 
-
     @bot.on(events.MessageDeleted)
-    async def deleted(event):
+    async def handler(event):
         for src, dst in last_replies[event.chat_id]:
             if src in event.deleted_ids:
                 await bot.delete_messages(event.chat_id, dst)
