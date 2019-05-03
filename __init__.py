5a18f3ea0b386b281e378a813fcc932da3341b30
painor <pi.oussama@gmail.com>
2019-05-03 16:09:07 +0100

    Add FAQ triggers (#2)

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import asyncio
-from . import autoadmin, randomkick, sed
+from . import autoadmin, randomkick, sed, faq
 
 
 # TODO Autodetect modules and init them all
@@ -8,5 +8,6 @@ def init(bot):
     asyncio.get_event_loop().run_until_complete(asyncio.wait([
         autoadmin.init(bot),
         randomkick.init(bot),
-        sed.init(bot)
+        sed.init(bot),
+        faq.init(bot)
     ]))
