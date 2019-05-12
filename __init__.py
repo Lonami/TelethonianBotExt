a313ad96aacceb0df03661617dcd02df1b3255af
Lonami Exo <totufals@hotmail.com>
2019-05-12 12:16:06 +0200

    Dynamically import plugins

--- __init__.py
+++ __init__.py
@@ -1,14 +1,26 @@
-import asyncio
-from . import autoadmin, randomkick, sed, faq, ref
+import importlib
+import os
+import warnings
 
 
-# TODO Autodetect modules and init them all
-# TODO Warn about errors if any fails to init
 def init(bot):
-    asyncio.get_event_loop().run_until_complete(asyncio.wait([
-        autoadmin.init(bot),
-        randomkick.init(bot),
-        sed.init(bot),
-        faq.init(bot),
-        ref.init(bot)
+    bot.loop.run_until_complete(start_plugins(bot, [
+        # Dynamically import
+        importlib.import_module(f'.', f'{__name__}.{file[:-3]}')
+
+        # All the files in the current directory
+        for file in os.listdir(os.path.dirname(__file__))
+
+        # If they start with a letter and are Python files
+        if file[0].isalpha() and file.endswith('.py')
     ]))
+
+
+async def start_plugins(bot, plugins):
+    for plugin in plugins:
+        p_init = getattr(plugin, 'init', None)
+        if callable(p_init):
+            try:
+                await p_init(bot)
+            except Exception as e:
+                warnings.warn(f'Failed to load {plugin.__name__}: {type(e)} ({e})')
