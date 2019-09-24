87adee82a96ba3a4d5ba3f87e868eade8abf5f22
Lonami <totufals@hotmail.com>
2019-09-24 15:43:16 +0200

    Fix #faq command not replying to messages

--- faq.py
+++ faq.py
@@ -24,5 +24,6 @@ async def init(bot):
         """#faq: Tell the user that their problem is in the faq."""
         await event.delete()
         await event.respond(
-            f"You can find a solution to your problem in [Telethon's FAQ]({FAQ_URL})"
+            f"You can find a solution to your problem in [Telethon's FAQ]({FAQ_URL})",
+            reply_to=event.reply_to_msg_id
         )
