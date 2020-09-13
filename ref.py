4e12f39a7010dbd19e3ab8bd0fc0ad58e8999be7
Lonami Exo <totufals@hotmail.com>
2020-09-13 10:59:45 +0200

    Support querying events

--- ref.py
+++ ref.py
@@ -2,6 +2,7 @@ import difflib
 import re
 import sys
 import urllib.parse
+from types import ModuleType
 
 from telethon import TelegramClient, custom, events
 from telethon.extensions import markdown
@@ -12,6 +13,7 @@ DOCS_MESSAGE = (
     'https://docs.telethon.dev/en/latest/'
     'modules/custom.html#telethon.tl.custom.message.Message.'
 )
+DOCS_EVENTS = 'https://docs.telethon.dev/en/latest/modules/events.html#'
 
 COMMON_WORDS = re.compile(
     r'\b('
@@ -21,16 +23,16 @@ COMMON_WORDS = re.compile(
     r')\b', re.IGNORECASE
 )
 
-def search_attr(cls, query, threshold=0.6):
+def search_attr(cls, query, threshold=0.6, func=None):
     query = COMMON_WORDS.sub('', query)
-    query = re.sub(' {2,}', ' ', query).strip()
+    query = re.sub(' {2,}', ' ', query).strip().casefold()
+    func = func or (lambda n: not n.startswith('_'))
 
     seq = difflib.SequenceMatcher(b=query, autojunk=False)
     scores = []
-    for n in dir(cls):
-        if not n.startswith('_'):
-            seq.set_seq1(n)
-            scores.append((n, seq.ratio()))
+    for n in filter(func, dir(cls)):
+        seq.set_seq1(n.casefold())
+        scores.append((n, seq.ratio()))
 
     scores.sort(key=lambda t: t[1], reverse=True)
     if threshold is None:
@@ -51,11 +53,19 @@ def attr_fullname(cls, n):
 
 def get_docs_message(kind, query):
     kind = kind.lower()
-    cls = {'client': TelegramClient, 'msg': custom.Message}[kind]
 
-    attr = search_attr(cls, query.lower())
+    func = None
+    if kind == 'client':
+        cls = TelegramClient
+    elif kind == 'msg':
+        cls = custom.Message
+    elif kind == 'event':
+        cls = events
+        func = func=lambda n: n[0].isupper()
+
+    attr = search_attr(cls, query, func=func)
     if not attr:
-        return f'No such method "{query}" :/'
+        return f'No such name "{query}" :/'
 
     name = attr
     if kind == 'client':
@@ -64,6 +74,10 @@ def get_docs_message(kind, query):
     elif kind == 'msg':
         name = f'Message.{name}'
         url = DOCS_MESSAGE
+    elif kind == 'event':
+        attr = f'{getattr(cls, attr).__module__}.{attr}'
+        name = f'events.{name}'
+        url = DOCS_EVENTS
     else:
         return f'No documentation for "{kind}"'
 
@@ -71,9 +85,9 @@ def get_docs_message(kind, query):
 
 
 async def init(bot):
-    @bot.on(events.NewMessage(pattern='(?i)#(client|msg) (.+)', forwards=False))
+    @bot.on(events.NewMessage(pattern='(?i)#(client|msg|event) (.+)', forwards=False))
     async def handler(event):
-        """#client or #msg query: Looks for the given attribute in RTD."""
+        """#client, #msg or #event query: Looks for the given attribute in RTD."""
         await event.delete()
 
         await event.respond(
@@ -101,7 +115,7 @@ async def init(bot):
         builder = event.builder
         result = None
         query = event.text.lower()
-        m = re.match('(client|msg).(.+)', query)
+        m = re.match('(client|msg|event).(.+)', query)
         if m:
             text = get_docs_message(m.group(1), m.group(2))
             query = markdown.parse(text)[0]
