bf08b8ba245991e46fa2fcc9866bd41ab88fc94a
Lonami Exo <totufals@hotmail.com>
2020-09-18 10:04:57 +0200

    More sophisticated init to support extra kwargs in plugins

--- __init__.py
+++ __init__.py
@@ -4,13 +4,14 @@ import os
 import warnings
 import logging
 import time
+import inspect
 
 
 async def init(bot):
     logging.basicConfig(level=logging.WARNING)
     logging.getLogger('asyncio').setLevel(logging.ERROR)
 
-    await start_plugins(bot, [
+    plugins = [
         # Dynamically import
         importlib.import_module(f'.', f'{__name__}.{file[:-3]}')
 
@@ -19,20 +20,44 @@ async def init(bot):
 
         # If they start with a letter and are Python files
         if file[0].isalpha() and file.endswith('.py')
-    ])
+    ]
+
+    # Keep a mapping of module name to module for easy access inside the plugins
+    modules = {m.__name__: m for m in plugins}
+
+    # All kwargs provided to get_init_args are those that plugins may access
+    to_init = (get_init_args(plugin, bot=bot, modules=modules) for plugin in plugins)
+
+    # Plugins may not have a valid init so those need to be filtered out
+    await asyncio.gather(*(filter(None, to_init)))
 
 
-async def _init_plugin(bot, plugin):
+def get_init_args(plugin, **kwargs):
     p_init = getattr(plugin, 'init', None)
-    if callable(p_init):
-        try:
-            logging.warning(f'Loading plugin {plugin.__name__}…')
-            start = time.time()
-            await p_init(bot)
-            took = time.time() - start
-            logging.warning(f'Loaded plugin {plugin.__name__} (took {took:.2f}s)')
-        except Exception:
-            logging.exception(f'Failed to load plugin {plugin}')
+    if not callable(p_init):
+        return
+
+    result_kwargs = {}
+    sig = inspect.signature(p_init)
+    for param in sig.parameters:
+        if param.name in kwargs:
+            result_kwargs[param.name] = kwargs[param.name]
+        else:
+            logging.error('Plugin %s has unknown init parameter %s', plugin.__name__, param.__name__)
+            return
+
+    return plugin, result_kwargs
+
+
+async def _init_plugin(plugin, kwargs):
+    try:
+        logging.warning(f'Loading plugin {plugin.__name__}…')
+        start = time.time()
+        await plugin.init(**kwargs)
+        took = time.time() - start
+        logging.warning(f'Loaded plugin {plugin.__name__} (took {took:.2f}s)')
+    except Exception:
+        logging.exception(f'Failed to load plugin {plugin}')
 
 
 async def start_plugins(bot, plugins):
