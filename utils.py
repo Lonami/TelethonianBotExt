332b773947f06c0f69997218c3e110acae357c35
x0x8x <5467132+x0x8x@users.noreply.github.com>
2020-10-09 21:32:32 +0300

    Update utils.py
    I don't know actually why it's even was unidecode in the first place which is tries to represent an Unicode characters in ASCII chars....
    consider to remove utils.py itself and `utils.get_display_name(entity)` part move to the haste.py

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-import unidecode
 from telethon import utils
-import re
 
 
 def get_display(entity):
@@ -8,10 +6,9 @@ def get_display(entity):
     Returns the display name of entity, safe to use in HTML strings.
     """
     if not entity:
-        return 'A user'
+        return "A user"
 
-    return re.sub(
-        r'[^\w\s]',
-        '',
-        unidecode.unidecode(utils.get_display_name(entity))
-    ).strip() or f'{entity.__class__.__name__} {entity.id}'
+    return (
+        utils.get_display_name(entity).strip()
+        or f"{entity.__class__.__name__} {entity.id}"
+    )
