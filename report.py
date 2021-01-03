cb76b30d960834b2bab211032c5920960fefcba4
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-01-03 19:58:29 +0530

    add 10 minutes report time for one user
    code copied from convproxy.py

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+import time
 from telethon.tl.types import (
     ChannelParticipantsAdmins,
     MessageEntityEmail,
@@ -10,6 +11,9 @@ from telethon.tl.types import (
 
 async def init(bot):
     # utils = modules['utils']
+    
+    reporting_time = {}  # user id to time they asked
+    reporting_delay = 10 * 60
 
     @bot.on(events.NewMessage(pattern='(?\/|#)report'))
     async def handler(event):
@@ -17,6 +21,10 @@ async def init(bot):
         #     event.sender_id,
         #     utils.get_display(event.sender)
         # )
+        delta = time.time() - reporting_time.get(event.sender_id, 0)
+        if delta < reporting_delay:
+            return
+
         reply_message = await event.get_reply_message()
         if not reply_message:
             return
@@ -42,4 +50,5 @@ async def init(bot):
             if not x.bot:
                 mentions += f"[\u2063](tg://user?id={x.id})"
         await reply_message.reply(mentions)
+        reporting_time[event.sender_id] = time.time()
         await event.delete()
