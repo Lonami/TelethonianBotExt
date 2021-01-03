5447e2f611a03637a990ac6d7d47f27b752cf8ad
Lonami <totufals@hotmail.com>
2021-01-03 20:53:53 +0100

    Apply suggestions from code review

--- report.py
+++ report.py
@@ -12,43 +12,31 @@ from telethon.tl.types import (
 async def init(bot):
     # utils = modules['utils']
     
-    reporting_time = {}  # user id to time they asked
-    reporting_delay = 10 * 60
+    reporting_time = {}  # chat_id to time of report
+    REPORT_DELAY = 10 * 60
 
     @bot.on(events.NewMessage(pattern='(?\/|#)report'))
     async def handler(event):
-        # mentions = '<a href="tg://user?id={}">{}</a> '.format(
-        #     event.sender_id,
-        #     utils.get_display(event.sender)
-        # )
-        delta = time.time() - reporting_time.get(event.sender_id, 0)
-        if delta < reporting_delay:
+        delta = time.time() - reporting_time.get(event.chat_id, 0)
+        if delta < REPORT_DELAY:
             return
 
         reply_message = await event.get_reply_message()
         if not reply_message:
             return
-        is_link = any(
-            isinstance(
-                entity, 
-                (
-                    MessageEntityEmail,
-                    MessageEntityMention,
-                    MessageEntityMentionName,
-                    MessageEntityTextUrl,
-                    MessageEntityUrl
-                )
-            ) for entity in (reply_message.entities or [])
-        )
-        if not (
-            reply_message.media or 
-            is_link
-        ):
+        has_links = any(isinstance(entity, (
+            MessageEntityEmail,
+            MessageEntityMention,
+            MessageEntityMentionName,
+            MessageEntityTextUrl,
+            MessageEntityUrl,
+        )) for entity in (reply_message.entities or ()))
+        if not reply_message.media and not has_links:
             return
         mentions = 'Reported to admins.'
         async for x in event.client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
             if not x.bot:
                 mentions += f"[\u2063](tg://user?id={x.id})"
         await reply_message.reply(mentions)
-        reporting_time[event.sender_id] = time.time()
+        reporting_time[event.chat_id] = time.time()
         await event.delete()
