34d2874ffa61a4b71cb8763e3f605bfc1131d7a5
Nitan Alexandru Marcel <nitan.marcel@protonmail.com>
2021-01-09 00:47:48 +0200

    Use stickers for join and leave actions in offtopic (#36)

--- welcome.py
+++ welcome.py
@@ -1,32 +1,49 @@
 from telethon import events, errors
 
+WELCOME = {}
 
-WELCOME = {
-    -1001109500936:
+MAIN_WELCOME = (
     'Hi and welcome to the group. Before asking any questions, **please** '
     'read [the docs](https://docs.telethon.dev/). Make sure you are '
     'using the latest version with `pip3 install -U telethon`, since most '
-    'problems have already been fixed in newer versions.',
+    'problems have already been fixed in newer versions.'
+)
 
-    -1001200633650:
-    'Welcome to the off-topic group. Feel free to talk, ask or test anything '
-    'here, politely. Check the description if you need to test more spammy '
-    '"features" of your or other people\'s bots (sed commands too).'
+WELCOME = {
+    -1001109500936: ((MAIN_WELCOME,), {}),
+    -1001200633650: ((), {'file': 'hello.webp'}),
+}
+
+FAREWELL = {
+    -1001200633650: ((), {'file': 'bye.webp'}),
 }
 
 
 async def init(bot):
     last_welcome = {}
 
-
     @bot.on(events.ChatAction)
     async def handler(event):
-        if event.user_joined:
-            if event.chat_id in last_welcome:
-                try:
-                    await last_welcome[event.chat_id].delete()
-                except errors.MessageDeleteForbiddenError:
-                    # We believe this happens when trying to delete old messages
-                    pass
+        chat_id = event.chat_id
+        joined = event.user_joined or event.user_added
+        left = event.user_left or event.user_kicked
+        welcome = WELCOME.get(chat_id, None)
+        farewell = FAREWELL.get(chat_id, None)
+        if not joined and not left:
+            return
+        if not welcome and not farewell:
+            return
 
-            last_welcome[event.chat_id] = await event.reply(WELCOME[event.chat_id])
+        if event.chat_id in last_welcome:
+            try:
+                await last_welcome[chat_id].delete()
+            except errors.MessageDeleteForbiddenError:
+                # We believe this happens when trying to delete old messages
+                pass
+
+        if joined and welcome:
+            args, kwargs = welcome
+        if left and farewell:
+            args, kwargs = farewell
+
+        await event.reply(*args, **kwargs)
