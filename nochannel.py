6d8658d3ea9a1abe40f028bee756fb1a389fe202
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2021-12-18 14:07:40 +0200

    nochannel.py: get the correct id of the message sender

--- nochannel.py
+++ nochannel.py
@@ -4,14 +4,13 @@ from telethon.tl.types import PeerChannel
 from telethon.utils import resolve_id
 
 CHATS = [-1001109500936, -1001200633650]
-PEER_CHATS = [resolve_id(i)[0] for i in CHATS]
 
 
 async def init(bot):
     @bot.on(events.NewMessage(chats=CHATS))
     async def handler(event):
-        if isinstance(event.peer_id, PeerChannel):
-            if event.peer_id.channel_id not in PEER_CHATS:
+        if isinstance(event.from_id, PeerChannel):
+            if event.from_id.channel_id not in CHATS:
                 try:
                     await event.delete()
                 except MessageDeleteForbiddenError:
