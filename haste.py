Lonami/TelethonianBotExt

commit ead4cd22c701deb3a63c380a594e904bcf1d3f25

Update haste.py (#57)

haste.py
@@ -61,18 +61,12 @@ async def init(bot, modules):
             code = msg.raw_text
             text = ''
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post('https://del.dog/documents',
-                                    data=code.encode('utf-8')) as resp:
-                if resp.status >= 300:
-                    async with session.post(
-                        "https://nekobin.com/api/documents", json={"content": code}) as r:
-                        if r.status >= 300:
-                            await sent.edit("Both del.dog and nekobin.com seem to be down… ( ^^')")
-                            return
-                        paste = f"nekobin.com/{(await r.json())['result']['key']}.py"
-                else:
-                    paste = f"del.dog/{(await resp.json())['key']}.py"
+        async with session.post(
+                "https://nekobin.com/api/documents", json={"content": code}) as r:
+            if r.status >= 300:
+                await sent.edit("nekobin.com seems to be down… ( ^^')")
+                return
+            paste = f"nekobin.com/{(await r.json())['result']['key']}.py"
 
         await asyncio.wait([
             msg.delete(),

commit dafaa1e315516ba6cd8e6031ad96ceff60dcf818

Fix haste again
It was broken back in #57.

haste.py
@@ -61,22 +61,23 @@ async def init(bot, modules):
             code = msg.raw_text
             text = ''
 
-        async with session.post(
-            "https://pasty.lus.pm/api/v2/pastes",
-            json={"content": code},
-            headers={
-                "User-Agent": "TelethonianBot/Version",
-                "Content-Type": "application/json",
-            }
-        ) as r:
-            if r.status >= 300:
-                await sent.edit("pasty seems to be down… ( ^^')")
-                return
-            paste = f"pasty.lus.pm/{(await r.json())['id']}"
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "https://pasty.lus.pm/api/v2/pastes",
+                json={"content": code},
+                headers={
+                    "User-Agent": "TelethonianBot/Version",
+                    "Content-Type": "application/json",
+                }
+            ) as r:
+                if r.status >= 300:
+                    await sent.edit("pasty seems to be down… ( ^^')")
+                    return
+                paste = f"pasty.lus.pm/{(await r.json())['id']}"
 
-        await asyncio.wait([
-            msg.delete(),
-            sent.edit(f'<a href="tg://user?id={msg.sender_id}">{name}</a> '
-                      f'said: {text} {paste}'
-                      .replace('  ', ' '), parse_mode='html')
-        ])
+            await asyncio.wait([
+                msg.delete(),
+                sent.edit(f'<a href="tg://user?id={msg.sender_id}">{name}</a> '
+                        f'said: {text} {paste}'
+                        .replace('  ', ' '), parse_mode='html')
+            ])